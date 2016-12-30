89aadbc83e8cbc1686b486574e29b37120ee98af
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 18:18:08 -0600

    marlin: build brillo_update_payload
    * For whatever reason this isn't getting built automatically
    
    Change-Id: Ie27c2050a7b14f04dd14f5ce1e6b86950b7d0df8
    
    Conflicts:
    	device-lineage.mk

--- device-lineage.mk
+++ device-lineage.mk
@@ -15,3 +15,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
+
+# Update engine
+PRODUCT_PACKAGES += \
+    brillo_update_payload
