0bbb66ef3fc3f4de53b2474c72f517f8987ed474
Nick Filmer <nick80835@gmail.com>
2018-04-28 12:55:57 -0400

    generalize kernel toolchain path
    Change-Id: I1a1c09253581f603a60bf306869a6b83a3e230b9
    Conflicts:
    	BoardConfigLineage.mk

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,6 +5,8 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
+KERNEL_TOOLCHAIN := ${ANDROID_BUILD_TOP}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-7.2.1/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
 
 # Telephony
 TARGET_PROVIDES_TELEPHONY_EXT := true
